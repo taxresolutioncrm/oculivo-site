aac96b6999976f0e1ed3fcf56ba6c22181e21730
RomyLabs <rcruz187@gmail.com>
2026-09-07 05:44:02 -0700

    Migrate restored Oculivo marketing site to GitHub/Cloudflare
    * Match Oculivo homepage to restored live design
    
    * Add Oculivo favicon for migrated site
    
    * Add Oculivo favicon and social metadata
    
    * Complete Oculivo production sitemap

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,8 +1,24 @@
-const urls = ['','features/','pricing/','privacy/','terms/','solutions/optometry/','solutions/ophthalmology/']
+const urls = [
+  '',
+  'features/',
+  'pricing/',
+  'privacy/',
+  'terms/',
+  'demo/',
+  'communications/',
+  'optical/',
+  'website-seo/',
+  'solutions/optometry/',
+  'solutions/ophthalmology/'
+]
+
 export function GET() {
   const body = '<?xml version="1.0" encoding="UTF-8"?>' +
     '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' +
-    urls.map(p => '<url><loc>https://oculivo.com/' + p + '</loc></url>').join('') +
+    urls.map((path) => '<url><loc>https://oculivo.com/' + path + '</loc></url>').join('') +
     '</urlset>'
-  return new Response(body, { headers: { 'Content-Type': 'application/xml' } })
+
+  return new Response(body, {
+    headers: { 'Content-Type': 'application/xml; charset=utf-8' }
+  })
 }
